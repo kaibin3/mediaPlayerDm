427957d4675447209c9bfa087acc610fc86d3978
wen.jie <wen.jie@phicomm.com>
2018-01-05 08:54:52 +0800

    phicomm 发现模块 titleBar

--- app/src/main/java/com/example/wenjie/mediaplayerdm/PhiFind/FindVideoActivity.java
+++ app/src/main/java/com/example/wenjie/mediaplayerdm/PhiFind/FindVideoActivity.java
@@ -2,44 +2,71 @@ package com.example.wenjie.mediaplayerdm.PhiFind;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.example.wenjie.mediaplayerdm.PhiFind.entry.FindCardInfo;
 import com.example.wenjie.mediaplayerdm.PhiFind.entry.FindVideoEntry;
 import com.example.wenjie.mediaplayerdm.R;
+import com.example.wenjie.mediaplayerdm.util.TitlebarUtils;
+import com.phicomm.widgets.PhiTitleBar;
 
 public class FindVideoActivity extends Activity implements FindVideoContract.View {
     private static final String TAG = "FindVideoActivity";
     public static final String VIDEO_ID = "video_id";
+    public static final String TITLE = "title";
 
+    private PhiTitleBar mPhiTitleBar;
     private VideoPlayView videoPlayView;
     private ListView listView;
 
     private FindVideoPresenter mPresenter;
     private RecommendAdapter mAdapter;
-    private String videoId;
+    private String mVideoId;
+    private String mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_video);
-        videoId = getIntent().getStringExtra(VIDEO_ID);
+        mVideoId = getIntent().getStringExtra(VIDEO_ID);
+        mTitle = getIntent().getStringExtra(TITLE);
         mPresenter = new FindVideoPresenter(this);
         init();
-        mPresenter.loadInfo(videoId);
+        mPresenter.loadInfo(mVideoId);
     }
 
     private void init() {
+        mPhiTitleBar = findViewById(R.id.phiTitleBar);
+        TitlebarUtils.initTitleBar(this, R.string.renyu);
+        mPhiTitleBar.addAction(new PhiTitleBar.TextAction("关闭") {
+            @Override
+            public void performAction(View view) {
+                finish();
+            }
+        });
         videoPlayView = findViewById(R.id.video_play_view);
         listView = findViewById(R.id.recommend_list_view);
         listView.setFocusable(false);
         mAdapter = new RecommendAdapter(this);
         listView.setAdapter(mAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FindCardInfo cardInfo = mAdapter.getItem(position);
+                videoPlayView.stopPlay();
+                videoPlayView.setPhotoUrl(cardInfo.getPhotoUrl());
+                videoPlayView.setVideoUrl(cardInfo.getVideoUrl());
+            }
+        });
     }
 
     @Override
     public void upDate(FindVideoEntry videoEntry) {
-        videoPlayView.setPhotoUrl(videoEntry.getPhotoUrl());
-        videoPlayView.setVideoUrl(videoEntry.getVideoUrl());
-        mAdapter.setData(videoEntry.getFindCardInfos());
+        videoPlayView.setMediaInfo(videoEntry.getMediaInfo());
+        //videoPlayView.setPhotoUrl(videoEntry.getPhotoUrl());
+        //videoPlayView.setVideoUrl(videoEntry.getVideoUrl());
+        mAdapter.setData(videoEntry.getRecommendInfo());
     }
 }
---
+++ app/src/main/java/com/example/wenjie/mediaplayerdm/util/TitlebarUtils.java
@@ -0,0 +1,105 @@
+package com.example.wenjie.mediaplayerdm.util;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.TypedValue;
+import android.view.View;
+
+import com.example.wenjie.mediaplayerdm.R;
+import com.phicomm.widgets.PhiTitleBar;
+
+/**
+ * Created by long.xiong on 2017/2/28.
+ */
+
+public class TitlebarUtils {
+    public static void initTitleBar(final Activity activity, PhiTitleBar titleBar) {
+        if (null != titleBar) {
+            //titleBar.setTitle(strResid);
+            titleBar.setTitleColor(Color.WHITE);
+            titleBar.setTitleSize(TypedValue.COMPLEX_UNIT_PX,
+                    49);
+
+            setActivityImmersive(activity, titleBar);
+        }
+    }
+
+    public static void initTitleBar(final Activity activity, PhiTitleBar titleBar, int strResid) {
+        if (null != titleBar) {
+            titleBar.setTitle(strResid);
+            titleBar.setTitleColor(Color.WHITE);
+            titleBar.setTitleSize(TypedValue.COMPLEX_UNIT_PX,
+                    49);
+
+            setActivityImmersive(activity, titleBar);
+        }
+    }
+
+    public static void initTitleBar(final Activity activity, PhiTitleBar titleBar, String str) {
+        if (null != titleBar) {
+            titleBar.setTitle(str);
+            titleBar.setTitleColor(Color.WHITE);
+            titleBar.setTitleSize(TypedValue.COMPLEX_UNIT_PX,
+                    49);
+
+            setActivityImmersive(activity, titleBar);
+        }
+    }
+
+    public static void initTitleBar(final Activity activity, int strResid) {
+        PhiTitleBar phiTitleBar = (PhiTitleBar) activity.findViewById(R.id.phiTitleBar);
+        if (null != phiTitleBar) {
+            phiTitleBar.setTitle(strResid);
+            phiTitleBar.setTitleColor(Color.WHITE);
+            phiTitleBar.setTitleSize(TypedValue.COMPLEX_UNIT_PX,
+                    49);
+            phiTitleBar.setLeftImageResource(R.drawable.rewind);
+            phiTitleBar.setLeftClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    activity.finish();
+                }
+            });
+            setActivityImmersive(activity, phiTitleBar);
+        }
+    }
+
+
+    private static void setActivityImmersive(Activity activity, PhiTitleBar titleBar) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = activity.getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+            titleBar.setImmersive(true);
+        } else {
+            titleBar.setImmersive(false);
+        }
+        titleBar.setAllCaps(true);
+        ActionBar actionBar = activity.getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+
+    private static void setActivityImmersive(Activity activity, PhiTitleBar titleBar, int color) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = activity.getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            activity.getWindow().setStatusBarColor(color);
+            titleBar.setImmersive(true);
+        } else {
+            titleBar.setImmersive(false);
+        }
+        titleBar.setAllCaps(true);
+        ActionBar actionBar = activity.getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+}
