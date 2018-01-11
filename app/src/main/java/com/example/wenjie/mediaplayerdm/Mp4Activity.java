51efb68a8ac1c30ada3c2ca3cb729406ae6591e5
wen.jie <wen.jie@phicomm.com>
2018-01-11 08:46:59 +0800

    myApp

--- app/src/main/java/com/example/wenjie/mediaplayerdm/Mp4Activity.java
+++ app/src/main/java/com/example/wenjie/mediaplayerdm/Mp4Activity.java
@@ -4,31 +4,21 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.Log;
 
 public class Mp4Activity extends Activity {
 
-
-    MediaPlayFragment mediaPlayFragment = new MediaPlayFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i("PlayActivity.onCreate ", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.media_play_activity);
 
-
-
-        addFragment(mediaPlayFragment);
+        addFragment(new MediaPlayFragment());
     }
 
-
-    void addFragment(Fragment fragment){
+    private void addFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-        fragmentTransaction.add(R.id.play_fragment,fragment);
+        fragmentTransaction.add(R.id.play_fragment, fragment);
         fragmentTransaction.commit();
     }
-
-
-
 
 }
---
+++ app/src/main/java/com/example/wenjie/mediaplayerdm/util/WindowUtils.java
@@ -0,0 +1,60 @@
+package com.example.wenjie.mediaplayerdm.util;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by wen.jie on 2018/1/10.
+ */
+
+public class WindowUtils {
+
+    /**
+     * 全屏
+     * 在onCreate()方法中的super()和setContentView()两个方法之间加入
+     */
+    public static void requestFullScreen(Activity activity) {
+        activity.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
+        //隐藏actionBar
+        if (activity instanceof AppCompatActivity) {
+            android.support.v7.app.ActionBar supportActionBar = ((AppCompatActivity) activity).getSupportActionBar();
+            if (supportActionBar != null) {
+                supportActionBar.hide();
+            }
+        } else {
+            ActionBar actionBar = activity.getActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
+        }
+
+    }
+
+    /**
+     * 是否是横屏
+     */
+    public static boolean isOrientationLand(Activity activity) {
+        return (activity.getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    /**
+     * 横屏
+     */
+    public static void setOrientationLand(Activity activity) {
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    /**
+     * 竖屏
+     */
+    public static void setOrientationPort(Activity activity) {
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
+
+}
