d5b2b69410937683796b3e0b4ece21d3675ed0dd
wen.jie <wen.jie@phicomm.com>
2018-01-12 10:28:56 +0800

    onLinePlayActivity

--- app/src/main/java/com/example/wenjie/mediaplayerdm/onLineViedio/OnLinePlayActivity.java
+++ app/src/main/java/com/example/wenjie/mediaplayerdm/onLineViedio/OnLinePlayActivity.java
@@ -1,32 +1,107 @@
 package com.example.wenjie.mediaplayerdm.onLineViedio;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 
 import com.example.wenjie.mediaplayerdm.R;
+import com.example.wenjie.mediaplayerdm.util.WindowUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class OnLinePlayActivity extends AppCompatActivity {
+    private static final String TAG = "OnLinePlayActivity";
+    private static final String FRAGMENT_TAG = "fragment_tag";
 
     OnLinePlayFragment onLinePlayFragment = new OnLinePlayFragment();
-
     VideoFragment videoFragment = new VideoFragment();
+
+    @BindView(R.id.ConstraintLayout)
+    ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
+        WindowUtils.requestFullScreen(this);
         setContentView(R.layout.activity_on_line_play);
 
-       // addFragment(onLinePlayFragment);
-        addFragment(new VideoFragment());
+        addFragment(onLinePlayFragment);
+        // addFragment(new VideoFragment());
+        ButterKnife.bind(this);
     }
+
 
     private void addFragment(Fragment fragment) {
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.add(R.id.play_fragment, fragment);
+        if (fragment.isAdded()) {
+            transaction.show(fragment);
+        } else {
+            transaction.add(R.id.play_fragment, fragment, FRAGMENT_TAG);
+        }
         transaction.commit();
     }
 
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && WindowUtils.isOrientationLand(this)) {
+            return onLinePlayFragment.onKeyBack();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.d(TAG, "onConfigurationChanged: " + newConfig.orientation);
+        if (Configuration.ORIENTATION_LANDSCAPE == newConfig.orientation) {
+            setContentView(R.layout.activity_on_line_play);
+            ButterKnife.bind(this);
+        } else {
+            setContentView(R.layout.activity_on_line_play);
+            ButterKnife.bind(this); //切换屏幕后需要再ButterKnife.bind
+        }
+    }
+
+    private void reAddFragment(Fragment oldFragment, Fragment newFragment1) {
+        FragmentManager fragmentManager = getFragmentManager();
+        if (oldFragment != null) {
+            FragmentTransaction trans = fragmentManager.beginTransaction();
+            trans.remove(oldFragment);
+            Fragment newFragment = new OnLinePlayFragment();
+            if (newFragment != null) {
+                trans.add(R.id.play_fragment, newFragment1, FRAGMENT_TAG);
+            }
+            trans.commit();
+        }
+    }
+
+    private void reAddFragment1(Fragment oldFragment, Fragment newFragment1) {
+        FragmentManager fragmentManager = getFragmentManager();
+        if (oldFragment != null) {
+            FragmentTransaction trans = fragmentManager.beginTransaction();
+            trans.hide(oldFragment);
+            //  Fragment newFragment = new OnLinePlayFragment();
+            if (newFragment1 != null) {
+                trans.add(R.id.play_fragment, newFragment1, FRAGMENT_TAG);
+            }
+            trans.commit();
+        }
+    }
+
+    @OnClick(R.id.ConstraintLayout)
+    void change() {
+        WindowUtils.changeScreenOrientation(OnLinePlayActivity.this);
+    }
 
 }
